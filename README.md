a7260656048f987453cd8d4f76875dd28201ea6d
WuLiZeng <2230432084@qq.com>
2023-05-12 09:28:46 +0800

    变量 常量 数据类型

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GoLearn
-Go基础
+Go语言基础
+
+## 内容参考
+- Go语言中文网 [🚪](https://studygolang.com/subject/2)
---
+++ src/main/demo.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//需要注意的是 { 不能单独放在一行
+	fmt.Println("hello Go")
+
+	/*
+		Go 语言的基础组成有以下几个部分：
+		包声明
+		引入包
+		函数
+		变量
+		语句 & 表达式
+		注释
+	*/
+}
